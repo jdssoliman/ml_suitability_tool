cb3f934a5aafb0c432ef7997c5384faaae2b2b3a
Jed Soliman <127481875+jdssoliman@users.noreply.github.com>
2025-08-06 08:46:40 +0800

    Update 1_🏠_Main_App.py

--- ml_tool_v2/1_🏠_Main_App.py
+++ ml_tool_v2/1_🏠_Main_App.py
@@ -1,18 +1,14 @@
 import streamlit as st
 
 
-st.title("🚀 ML Suitability Tool")
+st.title("🚀 MLaunchPad")
 
 st.markdown("""
 Welcome! This application is designed to help you explore, analyze, and interact with your data seamlessly.  
-Use the sidebar on the left to navigate through the different pages of the app.
+Use the sidebar on the left to navigate through the different pages of the app. 
 
 ---
 
 👈 **Choose a tool or feature from the sidebar to get started.**  
 💡 You’ll find pages for data quality checks, visualizations, model evaluations, and more!
 """)
-
-# Optional footer note
-st.markdown("---")
-st.markdown("📌 *Built using Streamlit*")
